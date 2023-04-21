55c5d54a20fec25254db9afbcaf47d2b54580023
kathy2470 <mukisakathy@gmail.com>
2023-04-21 14:16:04 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /**
-  * main - prints alphabet in lower case
+  * main - prints alphabet in lowerCase
+  *
   * Return: 0 (Success)
   */
 int main(void)
 {
-	char lc;
-
-	for (lc = 'a'; lc < = 'z'; lc++)
-	{
-		putchar(lc);
-	}
-	putchar('\n');
-	return (0);
+int lowerCase = 'a';
+while (lowerCase < = 'z';
+}
+putchar (LowerCase);
+LowerCase += 1;
+{
+putchar('\n');
+return (0);
 }
