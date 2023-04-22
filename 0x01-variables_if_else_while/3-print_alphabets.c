4ad43d5d34c5eeaeca108fdbda8af54e7114ba6d
kathy2470 <mukisakathy@gmail.com>
2023-04-22 02:26:50 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,14 +3,14 @@
 /**
   * main - print the alphabet of lowercase and uppercase
   * Description: followed by a new line
-  * Result: 0 (Success)
+  * Return: 0 (Success)
   */
 int main(void)
 {
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < = 52; i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
