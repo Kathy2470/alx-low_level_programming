40f9ac63ef14c6f6e072dec90ced0ef41851e266
kathy2470 <mukisakathy@gmail.com>
2024-04-11 01:18:51 +0300

    update

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -2,15 +2,15 @@
 #define SEARCH_ALGOS_H
 
 #include <stddef.h>
-#include <math.h>
 
-/* Function prototypes */
+/* Function prototypes for array search algorithms */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+
 typedef struct listint_s
 {
     int n;
@@ -18,7 +18,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/* Jump search in a singly linked list */
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
 typedef struct skiplist_s
@@ -29,24 +28,5 @@ typedef struct skiplist_s
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
-
-
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct listint_s
-{
-    int n;
-    size_t index;
-    struct listint_s *next;
-} listint_t;
-
-listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
